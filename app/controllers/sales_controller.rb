2031cd93637415fd994045778d6d1f93f3908a98
cristian vidal <cristian.vidal.lopez@gmail.com>
2018-04-23 09:59:27 -0300

    ADD: activity finish

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -10,19 +10,25 @@ class SalesController < ApplicationController
   end
 
   def done
+    @sale = Sale.all.last
   end
 
-private
+  private
+
   def sales_params
     params.require(:sale).permit(:detail, :category, :value, :discount, :tax)
   end
 
   def prepare_sale(sale)
-    sale.cod = Random.rand(10...100000)
+    sale.cod = Random.rand(10...100_000)
     sale.value = (sales_params[:value].to_i) * (100 - sales_params[:discount].to_i)
-    if sales_params[:tax] == 0
-      sale.total = sale.value * 1,19
+    if sales_params[:tax].to_i == 0
+      sale.total = sale.value * 1.19
       sale.tax = 19
+    else
+      sale.tax = 0
+      sale.total = sale.value
     end
+    sale.save
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,6 +2,6 @@ class Sale < ApplicationRecord
   validates :cod, uniqueness: true
   validates :detail, presence: true
   validates :category, inclusion: { in: [1, 2, 3, 4, 5] }
-  validates :value, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-  validates :discount, numericality: { less_than_or_equal_to: 40, only_integer: true }
+  validates_numericality_of :value, greater_than_or_equal_to: 0, only_integer: true
+  validates_numericality_of :discount, less_than_or_equal_to: 40, only_integer: true
 end
